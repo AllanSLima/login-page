32dd2cb63e981179d72fb6366b8615d51c3b59ca
ravarti <146475737+ravarti@users.noreply.github.com>
2023-10-24 17:42:04 -0300

    Add files via upload

---
+++ page/login/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Autenticar</h1>
+    <form action="ldap.php" method="post">
+        <input type="text" name="user" placeholder="Nome de usuário" /><br>
+        <input type="password" name="senha" placeholder="Senha" /><br>
+        <input type="submit" value="Login" />
+    </form>
+</body>
+</html>
