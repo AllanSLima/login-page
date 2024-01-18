331b22fa57b2a76906345c8c7a12b81ab0b662ab
Allan Souza <geral.allanlima851@gmail.com>
2024-01-18 17:07:57 -0300

    Add files via upload

--- page/login/ldap.php
+++ page/login/ldap.php
@@ -5,16 +5,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pass = $_POST['senha']; // Obtém a senha do formulário
 
     if (!empty($user) && !empty($pass)) {
+        // Verifica se o domínio não está presente e adiciona o sufixo 'cohabsp\' se necessário
+        if (strpos($user, '@cohabsp.sp.gov.br') === false) {
+            $user = 'cohabsp\\' . $user;
+        } else {
+            // Remove o sufixo 'cohabsp\' se o domínio estiver presente
+            $user = str_replace('cohabsp\\', '', $user);
+        }
+    }
+
+    if (!empty($user) && !empty($pass)) {
         if (!($connect = @ldap_connect($server))) {
-            die("Não foi possível conectar ao servidor LDAP");
+            $error_message = "Não foi possível conectar ao servidor LDAP";
+            header("Location: index.php?error_message=" . urlencode($error_message));
+            exit();
         }
 
         if (!($bind = @ldap_bind($connect, $user, $pass))) {
-            // se não validar, exibe uma mensagem de erro
-            echo "Usuário ou senha inválidos";
+            $error_message = "Usuário ou senha inválidos";
+            header("Location: index.php?error_message=" . urlencode($error_message));
+            exit();
         } else {
-            // se validar, busca o usuário em um grupo específico
-            $user = 'allan.lima';
+            // Remove o sufixo 'cohabsp\' se estiver presente
+            $user = str_replace('cohabsp\\', '', $user);
+
+            // Remove o domínio '@cohabsp.sp.gov.br' se estiver presente
+            $user = strstr($user, '@', true) ?: $user;
             $samaccountname = $user;
 
             $original_dn = "OU=Departamentos,DC=cohabsp,DC=sp,DC=gov,DC=br";
@@ -25,28 +41,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $first = ldap_first_entry($connect, $res);
             $data = ldap_get_dn($connect, $first);
 
-
             if ($data) {
-                // Aqui você pode processar os dados do usuário encontrado, se necessário
-                echo "Usuário autenticado com sucesso. Dados: $data <br />";
-
                 $sr = ldap_search($connect, $data, $filter, $justthese);
                 $info = ldap_get_entries($connect, $sr);
 
-                echo $info["count"] . " entries returned. \n";
+                if ($info["count"] === 0) {
+                    $error_message = "Usuário não tem acesso a estas informações";
+                    ldap_close($connect); // Fechando a conexão
+                    header("Location: index.php?error_message=" . urlencode($error_message));
+                    exit();
+                } else {
+                    ldap_close($connect); // Fechando a conexão
+                    header("Location: http://localhost:8080/page/teste-main/sistema.php");
+                    exit();
+                }
             } else {
-                echo "Usuário não encontrado no LDAP.";
+                ldap_close($connect); // Fechando a conexão
+                $error_message = "Usuário não encontrado no LDAP.";
+                header("Location: index.php?error_message=" . urlencode($error_message));
+                exit();
             }
-
-            // Fechar a conexão LDAP
-            ldap_close($connect);
         }
     } else {
-        // Caso os campos estejam vazios
-        echo "Por favor, preencha tanto o nome de usuário quanto a senha.";
+        $error_message = "Por favor, preencha tanto o nome de usuário quanto a senha.";
+        header("Location: index.php?error_message=" . urlencode($error_message));
+        exit();
     }
 } else {
-    // Se o método da requisição não for POST
-    echo "Método inválido para lidar com as credenciais.";
+    $error_message = "Método inválido para lidar com as credenciais.";
+    header("Location: index.php?error_message=" . urlencode($error_message));
+    exit();
 }
 ?>
